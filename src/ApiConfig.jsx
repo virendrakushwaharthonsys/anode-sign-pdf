c87c24603c4f60396ecbc4cd2ab865574b825d15
Nirmal patidar <Nirmalpatidarr@gmail.com>
2024-05-18 15:45:13 +0530

    second commit

--- src/ApiConfig.jsx
+++ src/ApiConfig.jsx
@@ -1,1 +1,2 @@
-export const BASE_URL ="http://localhost:3008/uploads/signedPdf-1715953210440.pdf"
+export const BASE_URL = "http://localhost:3008/uploads/signedPdf-1715953210440.pdf";
+// export const BASE_URL = "./src/Asset/pdf/intake.pdf";
